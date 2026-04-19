401df4a1b11c1cbdfff6316b4fd6911763f78b51
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-19 16:03:50 +0200

    new file: 01-simple_shell_0.1.c

---
+++ 01-simple_shell_0.1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: (0), if sucess
+ */
+int main(void)
+{
+	char *line = NULL, *argv[2];
+	size_t len = 0;
+	ssize_t nread = 0;
+	int interactive = isatty(STDIN_FILENO);
+	pid_t pid;
+
+	while (1)
+	{
+		if (interactive)
+			printf("$ ");
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			free(line);
+			exit(0);
+		}
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+
+		argv[0] = line;
+		argv[1] = NULL;
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			continue;
+		}
+		if (pid == 0)
+		{
+			execve(argv[0], argv, environ);
+			perror("execve");
+			exit(1);
+		}
+		if (pid > 0)
+			wait(NULL);
+	}
+	free(line);
+	return (0);
+}
