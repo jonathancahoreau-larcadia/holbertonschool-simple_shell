9493d178e429f3bf5e7e6dbd7ae818d7b7b8ccc8
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-20 10:49:42 +0200

    empty line added, msg error modified c:44

--- 01-simple_shell_0.1.c
+++ 01-simple_shell_0.1.c
@@ -28,7 +28,8 @@ int main(void)
 
 		if (line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
-
+		if (line[0] == '\0')
+			continue;
 		argv[0] = line;
 		argv[1] = NULL;
 		pid = fork();
@@ -40,7 +41,7 @@ int main(void)
 		if (pid == 0)
 		{
 			execve(argv[0], argv, environ);
-			perror(argv[0]);
+			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
 			exit(1);
 		}
 		if (pid > 0)
