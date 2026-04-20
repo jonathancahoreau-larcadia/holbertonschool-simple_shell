12f891aa7ac2e220c02e3e04803fb8b3317524de
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-20 23:08:27 +0200

    add librairies in main.h

--- main.h
+++ main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
 extern char **environ;
 char **tokenize(const char *line);
 void free_tokens(char **args);
