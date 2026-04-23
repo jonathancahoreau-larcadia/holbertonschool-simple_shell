b7b8fe22fbd46fa80e3b604f3e29837364b5527b
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-23 16:40:55 +0200

    add function to manage error

--- exec.c
+++ exec.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
 /**
+ * run_child_process - fork, execve and return child's exit status
+ * @full: full path to executable
+ * @args: argument vector
+ * @prog: program name
+ * @line: line count
+ *
+ * Return: exit status of child
+ */
+int run_child_process(char *full, char **args, char *prog, int line)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execve(full, args, environ);
+		print_exec_error(prog, line, args[0]);
+		if (errno == EACCES)
+			_exit(126);
+		if (errno == EISDIR)
+			_exit(127);
+		_exit(126);
+	}
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (status);
+}
+/**
  * execute - run a command with correct error messages
  * @args: arguments
  * @prog: program name
@@ -10,10 +47,8 @@
  */
 int execute(char **args, char *prog, int line)
 {
-	pid_t pid;
 	char *full = find_path(args[0]);
-	int status;
-	
+
 	if (!full)
 	{
 		fprintf(stderr, "%s: %d: %s: not found\n",
@@ -34,23 +69,5 @@ int execute(char **args, char *prog, int line)
 		free(full);
 		return (1);
 	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(full);
-		return (1);
-	}
-	if (pid == 0)
-	{
-		execve(full, args, environ);
-		print_exec_error(prog, line, args[0]);
-		_exit(126);
-	}
-	else
-		waitpid(pid, &status, 0);
-	free(full);
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
-	return (status);
+	return (run_child_process(full, args, prog, line));
 }
--- main.h
+++ main.h
@@ -18,5 +18,6 @@ char *find_path(char *cmd);
 char *get_path_env(void);
 int execute(char **args, char *prog, int line);
 void print_exec_error(char *prog, int line, char *cmd);
+int run_child_process(char *full, char **args, char *prog, int line);
 
 #endif
